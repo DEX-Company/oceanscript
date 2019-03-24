e52a6d9c3f8363ff2f5638d4c86b39ceb5157d37
Mike <mike@mikera.net>
2019-03-24 11:47:22 +0800

    Add base ANode class

---
+++ src/main/java/sg/dex/oceanscript/ANode.java
@@ -0,0 +1,13 @@
+package sg.dex.oceanscript;
+
+import com.oracle.truffle.api.nodes.Node;
+
+/**
+ * Base class for OceanScript AST nodes
+ * 
+ * @author Mike
+ *
+ */
+public abstract class ANode extends Node {
+
+}
